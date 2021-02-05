6138ec4dd0df094c35b37ca3b656597f8274d0f0
MaZy <mazyger@googlemail.com>
2021-02-05 18:47:42 +0100

    EventListener<T> was missing EventAction

--- EventListener.cs
+++ EventListener.cs
@@ -7,12 +7,25 @@ namespace Maz.Unity.EventFramework
 {
 	public abstract class EventListener<T> : MonoBehaviour
 	{
+		public EventAction<T> Event;
+
 		public UnityEvent<T> Response = default;
+
+		private void OnEnable()
+		{
+			Event.RegisterListener(this);
+		}
+
+		private void OnDisable()
+		{
+			Event.UnregisterListener(this);
+		}
 
 		public void OnEventRaised(T value)
 		{
 			Response?.Invoke(value);
 		}
+
 
 	}
 
