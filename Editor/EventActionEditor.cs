425eeaed8e7162394140d5cce39c7a87e21b71e7
MaZy <mazyger@googlemail.com>
2021-02-07 22:12:40 +0100

    Not needed files changed

--- Editor/EventActionEditor.cs
+++ Editor/EventActionEditor.cs
@@ -3,114 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
-namespace Maz.Unity.EventFramework
+namespace Maz.Unity.EventFramework.Example
 {
-
-    //[CustomEditor(typeof(EventAction))]
-    public class EventActionEditor : Editor
+	[CustomPropertyDrawer(typeof(EventAction<>), true)]
+	public class EventAcionEditor : PropertyDrawer
     {
-		GUIContent m_IconToolbarMinus;
-		GUIContent m_IconToolbarPlus;
-
-		//EventAction eventListenerTarget;
-
-		SerializedProperty isIntEventCollapsedProperty;
-		SerializedProperty isFloatEventCollapsedProperty;
-		SerializedProperty isDoubleEventCollapsedProperty;
-		SerializedProperty isStringEventCollapsedProperty;
-		SerializedProperty isGameEventCollapsedProperty;
-
-		private void OnEnable()
+		const float HEIGHT = 40;
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			//eventListenerTarget = target as EventAction;
-
-			m_IconToolbarMinus = new GUIContent(EditorGUIUtility.IconContent("Toolbar Minus"));
-			m_IconToolbarPlus = new GUIContent(EditorGUIUtility.IconContent("Toolbar Plus"));
-
-			isIntEventCollapsedProperty = serializedObject.FindProperty("isIntEventCollapsed");
-			isFloatEventCollapsedProperty = serializedObject.FindProperty("isFloatEventCollapsed");
-			isDoubleEventCollapsedProperty = serializedObject.FindProperty("isDoubleEventCollapsed");
-			isStringEventCollapsedProperty = serializedObject.FindProperty("isStringEventCollapsed");
-			isGameEventCollapsedProperty = serializedObject.FindProperty("isGameEventCollapsed");
-		}
-
-		public override void OnInspectorGUI()
-		{
-
-			var IntEventProperty = serializedObject.FindProperty("IntEvent");
-			isIntEventCollapsedProperty.boolValue = DrawCustomFoldout(IntEventProperty, isIntEventCollapsedProperty.boolValue);
-
-			EditorGUILayout.Space();
-
-			var FloatEventProperty = serializedObject.FindProperty("FloatEvent");
-			isFloatEventCollapsedProperty.boolValue = DrawCustomFoldout(FloatEventProperty, isFloatEventCollapsedProperty.boolValue);
-
-			EditorGUILayout.Space();
-
-			var DoubleEventProperty = serializedObject.FindProperty("DoubleEvent");
-			isDoubleEventCollapsedProperty.boolValue = DrawCustomFoldout(DoubleEventProperty, isDoubleEventCollapsedProperty.boolValue);
-
-			EditorGUILayout.Space();
-
-			var StringEventProperty = serializedObject.FindProperty("StringEvent");
-			isStringEventCollapsedProperty.boolValue = DrawCustomFoldout(StringEventProperty, isStringEventCollapsedProperty.boolValue);
-
-			EditorGUILayout.Space();
-
-			var GameEventProperty = serializedObject.FindProperty("GameEvent");
-			isGameEventCollapsedProperty.boolValue = DrawCustomFoldout(GameEventProperty, isGameEventCollapsedProperty.boolValue);
-
-			EditorGUILayout.Space();
-
-
-
-			serializedObject.ApplyModifiedProperties();
-		}
-
-		bool DrawCustomFoldout(SerializedProperty property, bool show)
-		{
-
-			GUIStyle originBoxStyle = GUI.skin.box;
-			originBoxStyle.alignment = TextAnchor.MiddleLeft;
-			GUIStyle style = GUI.skin.FindStyle("RL Header");
-
-			System.Type[] typeArguments = default;
-			if (property.propertyType == SerializedPropertyType.Generic)
+			if (!property.isExpanded)
 			{
-				var type = property.GetValue().GetType();
-				if(type.IsGenericType)
+				return EditorGUI.GetPropertyHeight(property, label);
+			}
+			return EditorGUI.GetPropertyHeight(property, label) + HEIGHT + 18f;
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.PropertyField(position, property, label, true);
+			var foldOut = position;
+			foldOut.height = 18;
+			property.isExpanded = EditorGUI.Foldout(foldOut, property.isExpanded, "", true);
+			EditorGUI.BeginProperty(position, label, property);
+
+			if (property.isExpanded)
+			{
+				position.y += 20f;
+
+				var content = new GUIContent();
+				var isPlaying = Application.isPlaying;
+
+				var s = new SerializedObject(property.objectReferenceValue);
+				SerializedProperty value = !isPlaying ? s.FindProperty("initialValue") : s.FindProperty("runtimeValue");
+
+				content.text = !isPlaying ? "    Initial Value" : "    Runtime Value" ;
+
+				s.UpdateIfRequiredOrScript();
+				EditorGUI.PropertyField(position, value, content, true);
+				s.ApplyModifiedProperties();
+
+
+				//position.height = 20;
+				position.height = 20f;
+				float height = EditorGUI.GetPropertyHeight(property, label);
+
+				position.y += height + 2f;
+
+				GUI.enabled = Application.isPlaying;
+				if (GUI.Button(position, "Raise"))
 				{
-					typeArguments = type.GetGenericArguments();
+					System.Type parentType = property.serializedObject.targetObject.GetType();
+					System.Reflection.FieldInfo fi = parentType.GetField(property.propertyPath);
 
+					var o = (EventActionBase)fi.GetValue(property.serializedObject.targetObject);
+
+					o.Raise();
 				}
-				//var genericType = type.GetGenericTypeDefinition();
 			}
-			GUIContent c = show ? m_IconToolbarMinus : m_IconToolbarPlus;
-
-			if(show)
-			{
-				EditorGUILayout.PropertyField(property);
-
-			} else {
-				GUILayout.BeginHorizontal(style);
-				var displayName = property.displayName;
-				GUILayout.Label(ReflectionHelper.GenerateGenericTypesAsString(property), GUILayout.ExpandWidth(true));
-				GUILayout.EndHorizontal();
-			}
-
-			Vector2 buttonIcon = GUIStyle.none.CalcSize(c);
-
-			Rect callbackRect = GUILayoutUtility.GetLastRect();
-			Rect buttonIconPos = new Rect(callbackRect.xMax - buttonIcon.x - 8, callbackRect.y + 1, buttonIcon.x, buttonIcon.y);
-			if (GUI.Button(buttonIconPos, c, GUIStyle.none))
-			{
-				show = !show;
-			}
-
-			return show;
+			EditorGUI.EndProperty();
 		}
-
-
 	}
 }
---
+++ Editor/EventActionEditorOld.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace Maz.Unity.EventFramework
+{
+
+    //[CustomEditor(typeof(EventAction))]
+    public class EventActionEditor : Editor
+    {
+		GUIContent m_IconToolbarMinus;
+		GUIContent m_IconToolbarPlus;
+
+		//EventAction eventListenerTarget;
+
+		SerializedProperty isIntEventCollapsedProperty;
+		SerializedProperty isFloatEventCollapsedProperty;
+		SerializedProperty isDoubleEventCollapsedProperty;
+		SerializedProperty isStringEventCollapsedProperty;
+		SerializedProperty isGameEventCollapsedProperty;
+
+		private void OnEnable()
+		{
+			//eventListenerTarget = target as EventAction;
+
+			m_IconToolbarMinus = new GUIContent(EditorGUIUtility.IconContent("Toolbar Minus"));
+			m_IconToolbarPlus = new GUIContent(EditorGUIUtility.IconContent("Toolbar Plus"));
+
+			isIntEventCollapsedProperty = serializedObject.FindProperty("isIntEventCollapsed");
+			isFloatEventCollapsedProperty = serializedObject.FindProperty("isFloatEventCollapsed");
+			isDoubleEventCollapsedProperty = serializedObject.FindProperty("isDoubleEventCollapsed");
+			isStringEventCollapsedProperty = serializedObject.FindProperty("isStringEventCollapsed");
+			isGameEventCollapsedProperty = serializedObject.FindProperty("isGameEventCollapsed");
+		}
+
+		public override void OnInspectorGUI()
+		{
+
+			var IntEventProperty = serializedObject.FindProperty("IntEvent");
+			isIntEventCollapsedProperty.boolValue = DrawCustomFoldout(IntEventProperty, isIntEventCollapsedProperty.boolValue);
+
+			EditorGUILayout.Space();
+
+			var FloatEventProperty = serializedObject.FindProperty("FloatEvent");
+			isFloatEventCollapsedProperty.boolValue = DrawCustomFoldout(FloatEventProperty, isFloatEventCollapsedProperty.boolValue);
+
+			EditorGUILayout.Space();
+
+			var DoubleEventProperty = serializedObject.FindProperty("DoubleEvent");
+			isDoubleEventCollapsedProperty.boolValue = DrawCustomFoldout(DoubleEventProperty, isDoubleEventCollapsedProperty.boolValue);
+
+			EditorGUILayout.Space();
+
+			var StringEventProperty = serializedObject.FindProperty("StringEvent");
+			isStringEventCollapsedProperty.boolValue = DrawCustomFoldout(StringEventProperty, isStringEventCollapsedProperty.boolValue);
+
+			EditorGUILayout.Space();
+
+			var GameEventProperty = serializedObject.FindProperty("GameEvent");
+			isGameEventCollapsedProperty.boolValue = DrawCustomFoldout(GameEventProperty, isGameEventCollapsedProperty.boolValue);
+
+			EditorGUILayout.Space();
+
+
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		bool DrawCustomFoldout(SerializedProperty property, bool show)
+		{
+
+			GUIStyle originBoxStyle = GUI.skin.box;
+			originBoxStyle.alignment = TextAnchor.MiddleLeft;
+			GUIStyle style = GUI.skin.FindStyle("RL Header");
+
+			System.Type[] typeArguments = default;
+			if (property.propertyType == SerializedPropertyType.Generic)
+			{
+				var type = property.GetValue().GetType();
+				if(type.IsGenericType)
+				{
+					typeArguments = type.GetGenericArguments();
+
+				}
+				//var genericType = type.GetGenericTypeDefinition();
+			}
+			GUIContent c = show ? m_IconToolbarMinus : m_IconToolbarPlus;
+
+			if(show)
+			{
+				EditorGUILayout.PropertyField(property);
+
+			} else {
+				GUILayout.BeginHorizontal(style);
+				var displayName = property.displayName;
+				GUILayout.Label(ReflectionHelper.GenerateGenericTypesAsString(property), GUILayout.ExpandWidth(true));
+				GUILayout.EndHorizontal();
+			}
+
+			Vector2 buttonIcon = GUIStyle.none.CalcSize(c);
+
+			Rect callbackRect = GUILayoutUtility.GetLastRect();
+			Rect buttonIconPos = new Rect(callbackRect.xMax - buttonIcon.x - 8, callbackRect.y + 1, buttonIcon.x, buttonIcon.y);
+			if (GUI.Button(buttonIconPos, c, GUIStyle.none))
+			{
+				show = !show;
+			}
+
+			return show;
+		}
+
+
+	}
+}
